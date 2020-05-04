b326eff6a38a360d4c88a4142265816b8f431022
CarlosZou <1271218214@qq.com>
2020-05-04 17:41:10 +0800

    启动 项目

---
+++ src/main/java/com/carlos/mall/MallApplication.java
@@ -0,0 +1,17 @@
+package com.carlos.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carlos
+ * @date 2020-05-04
+ */
+@SpringBootApplication
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}
